5868bff52b978f3c696c4f9221a8d374e1099eb8
DiamantONDO <anthodidiams@gmail.com>
2026-04-28 00:18:14 +0200

    Spring Boot application test scaffoldSpring Boot application test scaffold

---
+++ feeadback_webtech/src/test/java/com/feedback_webtech/feeadback_webtech/FeeadbackWebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedback_webtech.feeadback_webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeadbackWebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
