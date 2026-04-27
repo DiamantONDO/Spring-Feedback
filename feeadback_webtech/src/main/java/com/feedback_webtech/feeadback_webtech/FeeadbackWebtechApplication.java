4577a26f971c49ca3ecec9b82c8ab1e7581af1e0
DiamantONDO <anthodidiams@gmail.com>
2026-04-28 00:12:28 +0200

    Spring Boot application entry point

---
+++ feeadback_webtech/src/main/java/com/feedback_webtech/feeadback_webtech/FeeadbackWebtechApplication.java
@@ -0,0 +1,13 @@
+package com.feedback_webtech.feeadback_webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeadbackWebtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeadbackWebtechApplication.class, args);
+	}
+
+}
